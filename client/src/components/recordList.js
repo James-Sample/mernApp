7f8b607e50b4009b5630c23eb1ee8ca4f3d34ad6
James Sample <jamesgsample@gmail.com>
2023-02-06 17:51:43 +0000

    made a start on front end

---
+++ client/src/components/recordList.js
@@ -0,0 +1,91 @@
+// this code serves as a viewing component for our records, fetching records from the database through a GET method
+
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Record = (props) => (
+  <tr>
+    <td>{props.record.name}</td>
+    <td>{props.record.position}</td>
+    <td>{props.record.level}</td>
+    <td>
+      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>
+        Edit
+      </Link>
+      |
+      <button
+        className="btn btn-link"
+        onClick={() => {
+          props.deleteRecord(props.record._id);
+        }}
+      >
+        Delete
+      </button>
+    </td>
+  </tr>
+);
+
+export default function RecordList() {
+  const [records, setRecords] = useState([]);
+
+  // this method fetches the records from the database
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch(`http://localhost:5000/record/`);
+
+      if (!response.ok) {
+        const message = `An error occured: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const records = await response.json();
+      setRecords(records);
+    }
+
+    getRecords();
+
+    return;
+  }, [records.length]);
+
+  // this method will delete a record
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:5000/${id}`, {
+      method: "DELETE",
+    });
+
+    const newRecords = records.filter((el) => el._id !== id);
+    setRecords(newRecords);
+  }
+
+  // this method will map out the records on the table
+  function recordList() {
+    return records.map((record) => {
+      return (
+        <Record
+          record={record}
+          deleteRecord={() => deleteRecord(record._id)}
+          key={record._id}
+        />
+      );
+    });
+  }
+
+  // this section displays the table with recors of individuals
+  return (
+    <div>
+      <h3>Record List</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Names</th>
+            <th>Position</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{recordList()}</tbody>
+      </table>
+    </div>
+  );
+}
